e5524aa7f9916ea9a5f93ba2022a3ae1876f9860
rizwanzidi <157449071+rizwanzidi@users.noreply.github.com>
2024-02-07 20:47:08 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['923058420170', 'Rizwan zidi hacker', true],
-  [''], 
+  ['923058420170', 'Rizwan zidi hacker', true], 
   [''] 
 ] //Number of owners
 
